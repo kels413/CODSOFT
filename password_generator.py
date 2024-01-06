6be6dac9e0654ccd5080fda214097034eccc0c89
Kelvin Okoye <kelvinokoye413@gmail.com>
2024-01-06 23:13:10 +0100

    RPS GAME

--- password_generator.py
+++ password_generator.py
@@ -47,8 +47,7 @@ if __name__ == "__main__":
 
     if generated_password:
         print("Generated Password:", generated_password)
-ddddddddddddddddddddddddddddd
-
+dddddddddddddddddddddddddddddd
 
 
         
